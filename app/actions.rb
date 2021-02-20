f31c12574da85be1278c7532105050c638d2c29e
BCerki <briannac@shaw.ca>
2021-02-20 22:37:46 +0000

    21-02-20

--- app/actions.rb
+++ app/actions.rb
@@ -3,6 +3,7 @@ helpers do
         User.find_by(id: session[:user_id])
     end
 end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
@@ -36,10 +37,10 @@ post '/login' do
     username = params[:username]
     password = params[:password]
 
-    @user = User.find_by(username: username)
+    user = User.find_by(username: username)
 
-    if @user && @user.password == password
-        session[:user_id] = @user.id    
+    if user && user.password == password
+        session[:user_id] = user.id    
         redirect to('/')
      else
          @error_message = "Login failed."
@@ -50,4 +51,53 @@ end
 get '/logout' do
     session[:user_id] = nil
     redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+
+    @finstagram_post = FinstagramPost.new({photo_url: photo_url, user_id: current_user.id})
+    
+    if @finstagram_post.save
+        redirect(to('/'))
+    else
+        erb(:"finstagram_posts/new")
+    end
+end
+
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  comment = Comment.new({text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  comment.save
+
+  redirect(back)
+end
+
+post '/likes' do
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({finstagram_post_id: finstagram_post_id, user_id: current_user.id})
+
+    like.save
+    
+    redirect(back)
+end
+
+delete '/likes/:id' do 
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
 end
